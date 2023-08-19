29613cb43223d55558846f5f6af62180b86901cf
boarini <89103236+boarini@users.noreply.github.com>
2023-08-19 19:31:41 -0300

    versao-2.0

--- src/componentes/sections/presentation.js
+++ src/componentes/sections/presentation.js
@@ -1,13 +1,50 @@
 import styles from './Presentation.module.css'
 import ButtonA from '../elements/ButtonA'
+import { useState, useEffect } from 'react'
+
 
 function Presentation(){
+    const [text, setText] = useState('');
+    const toRotate = ['Matheus Boarini!', 'Desenvolvedor Full Stack'];
+    const [loop, setLoop] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const period = 100;
+    const [delta, setDelta] = useState(100)
+
+    useEffect(()=>{
+        let ticker = setInterval(()=>{
+            toType()
+        }, delta)
+        return()=> {clearInterval(ticker)}
+
+    }, [text])
+
+    const toType = () =>{
+        let i = loop % toRotate.length;
+        let fullText = toRotate[i]
+        let updatedText = isDeleting ? fullText.substring(0,text.length-1) : fullText.substring(0,text.length+1)
+
+        setText(updatedText);
+
+        if(!isDeleting && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        } else if(isDeleting && updatedText === ''){
+            setIsDeleting(false);
+            setDelta(period);
+            setLoop(loop+1);
+        }
+    }
+
     return(
         <div id="Presentation" className={styles.presentation}>
             <p><strong>Bem-vindo ao meu Portifólio</strong></p>
-            <h1>Olá, eu sou o Matheus Boarini!</h1>
-            <h4>Aficionado por tudo que tem a ver com tecnologia! Estou focado em me tornar <br/>
-                um Desenvolvedor Full Stack, e ...
+            <h1>Olá, eu sou {text}</h1>
+            <h4>Aficionado por tudo que tem a ver com tecnologia! Estou em uma joranada para me tornar <br/>
+                um Desenvolvedor Full Stack, e poder criar as melhores experiências digitais! Com uma base<br/>
+                em HTML, CSS e JavaScript, estou desenvolvendo interfaces de usuário intuitivas e responsivas.<br/>
+                No back end, estou mergulhando no mundo do Python e explorando o Microsoft SQL para dar vida<br/>
+                a funcionalidades. 
             </h4>
             <ButtonA link='http://github.com/boarini' text='Conecte-se comigo!'/>
           
